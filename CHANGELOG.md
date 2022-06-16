3fc9cd806f6752e83f71fb845e07b008f5110c04
regorxxx <regorxxx@protonmail.com>
2022-06-16 23:28:54 +0200

    - Opacity option. - Pressing 'Tab' key switches focus to buttons bar when it's opened -instead of following current position-, on first button. - Pressing 'Esc' key closes buttons bar. - 'CustomActions' option for custom buttons. Takes an array of objects with the same properties than the default ones (title, onClick, icon). Example is provided on html file. - 'disable' option properly switches global functionality (was not fully implemented on original code).

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -12,20 +12,30 @@
 
 ## [1.0.0] - 2022-06-16
 ### Added
-- Translate option.
-- Dictionary option: English, Spanish, French, Chinese, Hindi, Arabic, Malay, Russian, Bengali & Portugese. Follows browser config.
-- Google Image search option.
-- Print option.
+- Translate tool.
+- Dictionary tool: English, Spanish, French, Chinese, Hindi, Arabic, Malay, Russian, Bengali & Portugese. Follows browser config.
+- Google Image search tool.
+- Google Maps tool.
+- Print tool.
+- GitHub tool.
+- E-mail tool.
 - Scale configuration option to change overall size.
+- Opacity option.
 - Tooltip text on icons.
+- Pressing 'Tab' key switches focus to buttons bar when it's opened -instead of following current position-, on first button.
+- Pressing 'Esc' key closes buttons bar.
+- 'CustomActions' option for custom buttons. Takes an array of objects with the same properties than the default ones (title, onClick, icon). Example is provided on html file.
 ### Changed
 - General code cleanup.
 - Logo with new name (Xelection.js).
 - Demo page updated with new features, links and examples.
-- Popover is now animated.
+- Buttons bar is now animated.
+- Buttons bar is now displayed at bottom if it can not be drawn at top.
+- Icons are now buttons for all accessibility purposes (can be used with 'Tab' key).
 ### Removed
 ### Fixed
 - New popup windows can not access anymore storage from source web.
+- 'disable' option properly switches global functionality (was not fully implemented on original code).
 
 [Unreleased]: https://github.com/regorxxx/Xelection-js/compare/v1.0.0...HEAD
 [1.0.0]: https://github.com/regorxxx/Xelection-js/compare/69d2868...v1.0.0
--- xelection.js
+++ xelection.js
@@ -54,6 +54,7 @@ const Selection = (function() {
 		let text = '';
 		let bgColor = '#dc143c'; // Crimson
 		let iconColor = '#ffffff'; // White
+		let opacity = 1;
 		
 		let _icons = {};
 		let scale = 1;
@@ -63,6 +64,8 @@ const Selection = (function() {
 		let iconSize = iWH + buttonMargin;
 		let top = 0;
 		let left = 0;
+		
+		let isToolTipOnTop = true;
 		
 		const menuOptions = {
 			twitter:{
@@ -114,7 +117,7 @@ const Selection = (function() {
 					'<path id="svg_19" d="m20,18c-0.121,0 -0.242,-0.043 -0.337,-0.131c-0.363,-0.332 -3.558,-3.283 -4.126,-4.681c-0.104,-0.256 0.02,-0.547 0.275,-0.651c0.253,-0.103 0.547,0.019 0.651,0.275c0.409,1.007 2.936,3.459 3.875,4.319c0.204,0.187 0.217,0.502 0.031,0.707c-0.099,0.107 -0.234,0.162 -0.369,0.162z"/>'+
 					'</svg>'
 			},
-			dictionary: { // Merged: https://github.com/prateekkalra/Selection-js/pull/36
+			dictionary: {
 				enurl: 'https://dictionary.cambridge.org/dictionary/english/', //English
 				esurl: 'https://dle.rae.es/', //Spanish
 				frurl: 'https://www.linguee.com/english-french/search?source=french&query=', //French
@@ -132,21 +135,40 @@ const Selection = (function() {
 				'<path d="M18.666 10.137c0 .305-.251.556-.555.556H5.89C5.583 10.692 5.334 10.443 5.334 10.137s.251-.556.556-.556h12.223C18.417 9.581 18.666 9.832 18.666 10.137M14.717 12.364H5.89C5.583 12.364 5.334 12.613 5.334 12.92c0 .305.251 .555.556 .555h8.828c.305 0 .555-.251.555-.555C15.271 12.613 15.022 12.364 14.717 12.364M18.111 6.808H5.89C5.583 6.808 5.334 7.057 5.334 7.364c0 .305.251 .556.556 .556h12.223c.305 0 .555-.251.555-.556C18.666 7.057 18.417 6.808 18.111 6.808M22.555 3.469v13.333c0 .305-.251.556-.556.556H11.165l-3.772 3.728c-.156.155-.947.396-.947-.395v-3.333H2.001c-.305 0-.556-.251-.556-.556v-13.333c0-.305.251-.556.556-.556h20C22.306 2.915 22.555 3.164 22.555 3.469M21.445 4.025H2.557v12.223H7.001c.305 0 .556.249 .556.555v2.559l2.989-2.953c.105-.103.245-.16.392-.16h10.508V4.025z"/>' +
 				'</svg>'
 			},
-			image: { // Merged: https://github.com/prateekkalra/Selection-js/pull/35
+			image: {
 				url: 'https://www.google.com/search?tbm=isch&q=',
 				title: 'Image Search',
 				onClick: imageButton,
 				icon:
 				'<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="' + iWH + '" viewBox="12 2 5 ' + iWH + '" height="' + iWH + '" class="selection__icon"><defs><clipPath id="id1"><path d="M 3.386719 2.902344 L 26.613281 2.902344 L 26.613281 26.121094 L 3.386719 26.121094 Z M 3.386719 2.902344 " clip-rule="nonzero"></path></clipPath></defs><g clip-path="url(#id1)"><path d="M 19.0625 11.03125 C 17.460938 11.03125 16.160156 9.726562 16.160156 8.125 C 16.160156 6.523438 17.460938 5.222656 19.0625 5.222656 C 20.664062 5.222656 21.964844 6.523438 21.964844 8.125 C 21.964844 9.726562 20.664062 11.03125 19.0625 11.03125 Z M 23.476562 10.902344 C 24.03125 10.007812 24.347656 8.960938 24.277344 7.8125 C 24.125 5.316406 22.140625 3.203125 19.667969 2.9375 C 16.507812 2.585938 13.835938 5.039062 13.835938 8.125 C 13.835938 11.015625 16.171875 13.351562 19.050781 13.351562 C 20.074219 13.351562 21.023438 13.046875 21.824219 12.539062 L 24.625 15.335938 C 25.078125 15.789062 25.820312 15.789062 26.273438 15.335938 C 26.726562 14.882812 26.726562 14.140625 26.273438 13.6875 Z M 19.0625 21.480469 L 8.632812 21.480469 C 8.148438 21.480469 7.878906 20.921875 8.183594 20.539062 L 10.203125 17.949219 C 10.433594 17.660156 10.875 17.644531 11.109375 17.9375 L 12.917969 20.121094 L 15.648438 16.613281 C 15.878906 16.3125 16.34375 16.3125 16.566406 16.625 L 19.527344 20.5625 C 19.816406 20.933594 19.539062 21.480469 19.0625 21.480469 Z M 21.964844 17.996094 L 21.964844 22.640625 C 21.964844 23.28125 21.445312 23.800781 20.804688 23.800781 L 6.871094 23.800781 C 6.234375 23.800781 5.707031 23.28125 5.707031 22.640625 L 5.707031 8.707031 C 5.707031 8.070312 6.234375 7.542969 6.871094 7.542969 L 10.378906 7.542969 C 11.015625 7.542969 11.539062 7.023438 11.539062 6.382812 C 11.539062 5.746094 11.015625 5.222656 10.378906 5.222656 L 5.707031 5.222656 C 4.433594 5.222656 3.386719 6.269531 3.386719 7.542969 L 3.386719 23.800781 C 3.386719 25.078125 4.433594 26.121094 5.707031 26.121094 L 21.964844 26.121094 C 23.242188 26.121094 24.285156 25.078125 24.285156 23.800781 L 24.285156 17.996094 C 24.285156 17.359375 23.765625 16.832031 23.125 16.832031 C 22.488281 16.832031 21.964844 17.359375 21.964844 17.996094 " fill-rule="nonzero"></path></g></svg>'
 			},
-			print: { // Merged: https://github.com/prateekkalra/Selection-js/pull/20
+			map: {
+				url: 'https://www.google.com/maps/search/?api=1&query=',
+				title: 'Search on map',
+				onClick: mapButton,
+				icon: '<svg xmlns="http://www.w3.org/2000/svg" width="' + iWH + '" height="' + iWH + '" viewBox="0 0 ' + iWH + ' ' + iWH + '" enable-background="new 0 0 ' + iWH + ' ' + iWH + '" class="selection__icon">'+ '<path d="M17.5 1C15.711 1 14.135 1.873 13.13 3.2L3.2 3.2C1.984 3.2 1 4.185 1 5.4L1 20.8C1 20.996 1.034 21.181 1.082 21.363L12.715 9.729C13.014 10.457 13.36 11.147 13.697 11.792 13.705 11.807 13.711 11.821 13.719 11.837L12.455 13.1 20.718 21.363C20.766 21.181 20.8 20.996 20.8 20.8L20.8 12.726C20.974 12.355 21.161 11.979 21.361 11.59 22.13 10.088 23 8.385 23 6.5 23 3.467 20.533 1 17.5 1zM17.47 3.232C19.293 3.232 20.77 4.71 20.77 6.532 20.769 9.278 17.915 12.033 17.915 14.93 17.915 15.157 17.694 15.326 17.468 15.326 17.241 15.326 17.055 15.125 17.055 14.898 17.055 12.001 14.17 9.531 14.17 6.532 14.17 4.71 15.647 3.232 17.47 3.232zM6.502 5.4C7.358 5.4 8.136 5.725 8.724 6.259L7.796 7.19C7.447 6.892 6.996 6.708 6.502 6.708 5.402 6.708 4.511 7.6 4.511 8.7 4.511 9.799 5.402 10.692 6.502 10.692 7.425 10.692 8.064 10.143 8.298 9.379L6.502 9.379 6.502 8.12 9.611 8.124C9.882 9.41 9.272 12 6.502 12 4.678 12 3.2 10.523 3.2 8.7 3.2 6.877 4.678 5.4 6.502 5.4zM17.5 5.4A1.1 1.1 0 0 0 16.4 6.5 1.1 1.1 0 0 0 17.5 7.6 1.1 1.1 0 0 0 18.6 6.5 1.1 1.1 0 0 0 17.5 5.4zM10.9 14.655 2.637 22.918C2.819 22.966 3.004 23 3.2 23L18.6 23C18.796 23 18.981 22.966 19.163 22.918L10.9 14.655z"></path></svg>'
+			},
+			github: {
+				url: 'https://github.com/search?q=',
+				title: 'Search on GitHub',
+				onClick: githubButton,
+				icon: '<svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 16 16" enable-background="new 0 0 ' + iWH + ' ' + iWH + '" width="' + iWH + '" height="' + iWH + '" class="selection__icon"><path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path></svg>'
+			},
+			email: {
+				url: 'mailto:?',
+				title: 'Send by e-mail',
+				onClick: emailButton,
+				icon:
+				'<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 15 17" enable-background="new 0 0 ' + iWH + ' ' + iWH + '" width="' + iWH + '" height="' + iWH + '" class="selection__icon"><path d="M14.5 2h-13c-0.825 0-1.5 0.675-1.5 1.5v10c0 0.825 0.675 1.5 1.5 1.5h13c0.825 0 1.5-0.675 1.5-1.5v-10c0-0.825-0.675-1.5-1.5-1.5zM6.23 8.6l-4.23 3.295v-7.838l4.23 4.543zM2.756 4h10.488l-5.244 3.938-5.244-3.938zM6.395 8.777l1.605 1.723 1.605-1.723 3.29 4.223h-9.79l3.29-4.223zM9.77 8.6l4.23-4.543v7.838l-4.23-3.295z"/></svg>'
+			},
+			print: {
 				title: 'Print',
 				onClick: printButton,
 				icon:
 				  '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ' + iWH + ' ' + iWH + '" enable-background="new 0 0 ' + iWH + ' ' + iWH + '" width="' + iWH + '" height="' + iWH + '" class="selection__icon"><g id="surface1"><path style=" stroke:none;fill-rule:nonzero;" d="M 6 18.75 L 6 23.25 L 18 23.25 L 18 15.75 L 6 15.75 Z M 7.5 17.25 L 16.5 17.25 L 16.5 18.75 L 7.5 18.75 Z M 7.5 20.25 L 16.5 20.25 L 16.5 21.75 L 7.5 21.75 Z M 7.5 20.25 "/><path style=" stroke:none;fill-rule:nonzero;" d="M 18 5.25 L 18 0.75 L 6 0.75 L 6 8.25 L 18 8.25 Z M 18 5.25 "/><path style=" stroke:none;fill-rule:nonzero;" d="M 21 5.25 L 19.5 5.25 L 19.5 9.75 L 4.5 9.75 L 4.5 5.25 L 3 5.25 C 1.5 5.25 0 6.75 0 8.25 L 0 15.75 C 0 17.25 1.5 18.75 3 18.75 L 4.5 18.75 L 4.5 14.25 L 19.5 14.25 L 19.5 18.75 L 21 18.75 C 22.5 18.75 24 17.25 24 15.75 L 24 8.25 C 24 6.75 22.5 5.25 21 5.25 Z M 21 5.25 "/></g></svg>'
 			}
 		};
-		const menu = Object.fromEntries([...Object.keys(menuOptions).map((item) => {return [item, true];}), ['disable', 'false']]);
+		const menu = Object.fromEntries([...Object.keys(menuOptions).map((item) => {return [item, true];}), ['disable', false], ['customActions', []]]);
 		
 		function facebookButton() {
 			let sharelink = window.location.href;
@@ -211,6 +233,21 @@ const Selection = (function() {
 			return false;
 		}
 		
+		function mapButton(){
+			popupwindow(this.url + encodeURIComponent(text));
+			return false;
+		}
+		
+		function githubButton() {
+			popupwindow(this.url + text, 'GitHub');
+			return false;
+		}
+		
+		function emailButton() {
+			window.location.href = this.url +'body=' + encodeURIComponent(text);
+			return false;
+		}
+		
 		function printButton() {
 			const selectwindow = window.open();
 			selectwindow.document.write(text);
@@ -226,7 +263,7 @@ const Selection = (function() {
 		}
 		
 		function newButton() {
-			return (new Button(this.icon, this.title, this.onClick.bind(this)));
+			return (new Button(this.icon, this.title, this.onClick.bind(this, text))); // Apply text arg for custom callbacks
 		}
 		
 		function appendIcons() {
@@ -238,6 +275,12 @@ const Selection = (function() {
 					count++;
 				}
 			})
+			 menu.customActions.forEach((item)=>{
+				if (item.icon !== void(0)) {
+					div.appendChild(newButton.apply(item));
+					count++;
+				}
+			 })
 			return {
 				icons: div,
 				length: count
@@ -246,9 +289,17 @@ const Selection = (function() {
 		
 		function setTooltipPosition() {
 			const position = selection.getRangeAt(0).getBoundingClientRect();
-			const DOCUMENT_SCROLL_TOP =
-				window.pageXOffset || document.documentElement.scrollTop || document.body.scrollTop;
-			top = position.top + DOCUMENT_SCROLL_TOP - iconSize - arrowSize;
+			// const DOCUMENT_SCROLL_TOP = window.pageXOffset || document.documentElement.scrollTop || document.body.scrollTop;
+			// top = position.top + DOCUMENT_SCROLL_TOP - iconSize - arrowSize;
+			// left = position.left + (position.width - iconSize * _icons.length) / 2;
+			const DOCUMENT_SCROLL_TOP = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
+			if (position.top >= iconSize) {
+				top = position.top + DOCUMENT_SCROLL_TOP - iconSize - arrowSize;
+				isToolTipOnTop = true;
+			} else {
+				top = position.top + DOCUMENT_SCROLL_TOP + position.height + arrowSize;
+				isToolTipOnTop = false;
+			}
 			left = position.left + (position.width - iconSize * _icons.length) / 2;
 		}
 		
@@ -275,20 +326,14 @@ const Selection = (function() {
 				'transition:all .2s ease-in-out;' +
 				'box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);' +
 				'z-index:99999;' +
+				'opacity:' + opacity + ';' +
 				'scale:' + scale + ';'
 				;
 			
-			div.animate( // Merged: https://github.com/prateekkalra/Selection-js/pull/43
-				[
-					// keyframes
-					{ transform: "translateY(100%) scale(0)" },
-					{ transform: "translateY(0) scale(100%)" },
-				],
-				{
-					// timing options
-					duration: 200,
-				}
-			)
+			div.animate(
+				[{transform: "translateY(100%) scale(0)"}, {transform: "translateY(0) scale(100%)"},], // keyframes
+				{duration: 200} // timing options
+			);
 			div.appendChild(_icons.icons);
 			
 			const arrow = document.createElement('div');
@@ -297,18 +342,44 @@ const Selection = (function() {
 				'border-left:' + arrowSize + 'px solid transparent;' +
 				'border-right:' + arrowSize + 'px solid transparent;' +
 				'border-top:' + arrowSize + 'px solid ' + bgColor + ';' +
-				'bottom:-' + (arrowSize - 1) + 'px;' +
+				(isToolTipOnTop ? 'bottom:-' + (arrowSize - 1) + 'px;' : 'top:-' + (arrowSize - 1) + 'px;transform:rotate(180deg);') +
 				'left:' + (iconSize * _icons.length / 2 - arrowSize) + 'px;' +
 				'width:0;' +
 				'height:0;' +
 				'scale:' + scale + ';'
 				;
-			
-			if (!menu.disable) {
-				div.appendChild(arrow);
-			}
+			div.appendChild(arrow);
 			
 			document.body.appendChild(div);
+			window.addEventListener('keydown', tooltipKeydown);
+		}
+		
+		function tooltipKeydown(e){
+			setTimeout(function tooltipKeydownTimeout() {
+				switch (e.keyCode) {
+					case 27: { // escape: close
+						removeTooltip();
+					}
+					case 9: { // Tab: set focus on buttons bar if it's not there
+						const tooltip = document.querySelector('.selection');
+						const buttons = tooltip.getElementsByTagName('button');
+						const focus = document.querySelector(':focus');
+						if (!Array.from(buttons).some((button) => {return focus === button;})) {
+							focusFirstButton();
+						}
+					}
+				}
+			}, 10);
+		}
+		
+		function removeTooltip(){
+			document.querySelector('.selection').remove();
+			window.removeEventListener('keydown', tooltipKeydown);
+		}
+		
+		function focusFirstButton(){
+			const tooltip = document.querySelector('.selection');
+			tooltip.getElementsByTagName('button')[0].focus();
 		}
 		
 		function attachEvents() {
@@ -320,26 +391,35 @@ const Selection = (function() {
 				return !!document.querySelector('.selection');
 			}
 
+			function getText() {
+				selection = window.getSelection();
+				text = selection.toString();
+				return text;
+			}
+
 			window.addEventListener(
 				'mouseup',
 				function() {
-				setTimeout(function mouseTimeout() {
-					if (hasTooltipDrawn()) {
-						if (hasSelection()) {
-							selection = window.getSelection();
-							text = selection.toString();
-							moveTooltip();
+					setTimeout(function mouseTimeout() {
+						if (menu.disable) {
+							if (hasTooltipDrawn()) {
+								removeTooltip();
+							}
 							return;
-						} else {
-							document.querySelector('.selection').remove();
 						}
-					}
-					if (hasSelection()) {
-						selection = window.getSelection();
-						text = selection.toString();
-						drawTooltip();
-					}
-				}, 10);
+						if (hasTooltipDrawn()) {
+							if (hasSelection()) {
+								getText();
+								moveTooltip();
+								return;
+							}
+							removeTooltip();
+						}
+						if (hasSelection()) {
+							getText();
+							drawTooltip();
+						}
+					}, 10);
 				},
 				false
 			);
@@ -354,7 +434,9 @@ const Selection = (function() {
 				} else if (option === 'iconColor') {
 					iconColor = options[option] || '#ffffff'; // White 
 				} else if (option === 'scale') {
-					scale = scale && isFinite(options[option]) ? options[option] : 1;
+					scale = typeof options[option] !== 'undefined' && isFinite(options[option]) ? options[option] : 1;
+				} else if (option === 'opacity') {
+					opacity = typeof options[option] !== 'undefined' && isFinite(options[option]) ? options[option] : 1;
 				} else {console.log('Option not recognized: ' + option);}
 			})
 			return this;
@@ -370,8 +452,8 @@ const Selection = (function() {
 	}
 	
 	function Button(icon, titleText, clickFn) {
-		const btn = document.createElement('div');
-		btn.style = 'display:inline-block;' + 'margin:7px;' + 'cursor:pointer;' + 'transition:all .2s ease-in-out;';
+		const btn = document.createElement('button');
+		btn.style = 'background-color:transparent;' + ' border:0px;' + 'margin:7px;' + 'transition:all .2s ease-in-out;';
 		btn.innerHTML = icon;
 		btn.title = titleText;
 		btn.onclick = clickFn;
